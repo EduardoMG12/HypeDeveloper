59edb423f0013754402115732d6d01078e84c5bc
Eduardo <eduardo@pop-os.localdomain>
2023-05-09 17:25:03 -0300

    feat(dinamicPage/Article):created article with optional components dependency to mock

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { articleMock, IArticle } from "../../../../data/mock";
-import { metadata } from "@/app/layout";
 import { Metadata } from "next";
+import Link from "next/link";
+import { Article as ArticleComponent } from "../../../components/Article";
 
 interface IParams {
   params: { id: string };
@@ -13,7 +14,40 @@ export async function generateMetadata({ params }: IParams): Promise<Metadata> {
 
 const Article = async ({ params }: IParams) => {
   const article: IArticle = await articleMock[parseInt(params.id) - 1];
-  return <div>{article.description}</div>;
+  return (
+    <>
+      <header className="flex w-full justify-center items-center h-[5.5rem] border-b border-white/10 ">
+        <Link href="/">
+          <h1 className="text-3xl">HypeDeveloper</h1>
+        </Link>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <ArticleComponent.Root>
+          <ArticleComponent.Title paragraphText={article.title} />
+          <ArticleComponent.PrincipalSection
+            paragraphText={article.textPrincipal}
+          />
+          {article.image ? (
+            <ArticleComponent.Image src={article.image} />
+          ) : null}
+          {article.textSecondary ? (
+            <ArticleComponent.SecondarySection
+              paragraphText={article.textSecondary}
+            />
+          ) : null}
+          {article.video ? (
+            <ArticleComponent.Video src={article.video} />
+          ) : null}
+          {article.textLeft || article.textRight ? (
+            <ArticleComponent.ContainerLeftAndRightContent
+              leftSection={article.textLeft ?? null}
+              rightSection={article.textRight ?? null}
+            />
+          ) : null}
+        </ArticleComponent.Root>
+      </main>
+    </>
+  );
 };
 
 export default Article;
