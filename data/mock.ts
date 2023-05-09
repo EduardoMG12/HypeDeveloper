f7a2a9b6e1bb51bf3833743856454071ed8073c9
Eduardo <eduardo@pop-os.localdomain>
2023-05-09 17:38:06 -0300

    refactor(mock/data): added new argument to interface, is thumbNail

--- data/mock.ts
+++ data/mock.ts
@@ -2,8 +2,11 @@ export interface IArticle {
     id: number;
     title: string;
     description: string;
+    thumbNail:string
     image?: string;
-    textNormal?: string;
+    video?: string;
+    textPrincipal: string;
+    textSecondary?: string;
     textLeft?: string;
     textRight?: string;
   }
@@ -11,10 +14,16 @@ export interface IArticle {
   export const articleMock: IArticle[] = [
     {
     id: 1,
-    title: 'Meu primeiro artigo example',
+    title: 'A internet não é brincadeira: o perigo das fake news e conteúdos enganosos',
     description: 'Este é o meu primeiro artigo sobre Next.js',
-   
-    textNormal: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    thumbNail:"https://images.unsplash.com/photo-1585995603666-5bd6b348de9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFrZW5ld3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    textPrincipal: `A internet é uma ferramenta poderosa que trouxe muitos benefícios para o mundo. Hoje em dia, é possível acessar uma grande quantidade de informações em segundos. No entanto, essa facilidade também traz riscos, especialmente quando se trata de fake news e conteúdos enganosos.
+
+    É importante lembrar que nem tudo o que está na internet é verdade. Infelizmente, muitas pessoas utilizam a rede para disseminar informações falsas, com o intuito de manipular e enganar os usuários. Além disso, há também conteúdos que são ilegais, como pornografia infantil e incitação à violência.
+    
+    Apesar de existirem leis processuais para combater esses problemas, é praticamente impossível regulamentar todas as informações que circulam na internet. Por isso, é responsabilidade de cada um de nós ter cuidado ao navegar na web.
+    
+    `,
     textLeft: 'Ut fermentum accumsan felis quis mollis.',
     textRight: 'Donec ornare elit sed magna volutpat bibendum.',
   },
@@ -22,8 +31,9 @@ export interface IArticle {
     id: 2,
     title: 'Meu segundo artigo example',
     description: 'Este é o meu segundo artigo sobre Next.js',
+    thumbNail: "",
     image: '',
-    textNormal: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    textPrincipal: 'lorem epsilumlorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilumlorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum lorem epsilum',
     textLeft: 'Ut fermentum accumsan felis quis mollis.',
     textRight: 'Donec ornare elit sed magna volutpat bibendum.',
   }]
